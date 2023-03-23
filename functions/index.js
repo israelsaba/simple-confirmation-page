65dcf7421c63d46b10320a0294c79ca097adf1b7
israelsaba <israelsaba@hotmail.com>
2023-03-23 17:50:15 -0300

    feat: Sends MixPanel data when clicking an add

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,20 @@
 const functions = require("firebase-functions");
+const Mixpanel = require('mixpanel');
+const url = require('url');
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
 
 
-const url = require('url');
-const querystring = require('querystring');
 
-exports.sendMpDataAndForward = functions.https.onRequest((request, response) => {
+exports.sendMpAdsDataAndForward = functions.https.onRequest((request, response) => {
     const parsedURL = url.parse(request.url, true);
-    const queryData = querystring.parse(parsedURL.query);
-    response.status(200).send(`Data is: ${JSON.stringify(queryData, null, 2)} URL parsed is ${JSON.stringify(parsedURL, null, 2)}`);
+    const queryParams = parsedURL.query
+    const mixpanel = Mixpanel.init('73b777655a43f335b97bc9bb8cbf4700', {debug: true});
+    const combinedObj = Object.assign(parsedURL, queryParams); 
+    mixpanel.track('Ad clicked', combinedObj);
+    const forwardURL = `https://investorhub.app`
+    response.redirect(forwardURL);
+    //response.status(200).send(`URL parsed is ${JSON.stringify(parsedURL, null, 2)}`);
 });
