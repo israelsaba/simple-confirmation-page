e9e3a860a59c928241d80655c45819af7cca6608
israelsaba <israelsaba@hotmail.com>
2023-03-23 18:33:50 -0300

    feat: Creates function to send Mixpanel data of subs

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,6 @@ const url = require('url');
 // // https://firebase.google.com/docs/functions/get-started
 //
 
-
-
 exports.sendMpAdsDataAndForward = functions.https.onRequest((request, response) => {
     const parsedURL = url.parse(request.url, true);
     const queryParams = parsedURL.query
@@ -15,6 +13,17 @@ exports.sendMpAdsDataAndForward = functions.https.onRequest((request, response) 
     const combinedObj = Object.assign(parsedURL, queryParams); 
     mixpanel.track('Ad clicked', combinedObj);
     const forwardURL = `https://investorhub.app`
+    response.redirect(forwardURL);
+    //response.status(200).send(`URL parsed is ${JSON.stringify(parsedURL, null, 2)}`);
+});
+
+exports.sendMpAEmailConfirmationDataAndForward = functions.https.onRequest((request, response) => {
+    const parsedURL = url.parse(request.url, true);
+    const queryParams = parsedURL.query
+    const mixpanel = Mixpanel.init('73b777655a43f335b97bc9bb8cbf4700', {debug: true});
+    const combinedObj = Object.assign(parsedURL, queryParams); 
+    mixpanel.track('Ad clicked', combinedObj);
+    const forwardURL = `https://investorhub.app/obrigado-inscrito`
     response.redirect(forwardURL);
     //response.status(200).send(`URL parsed is ${JSON.stringify(parsedURL, null, 2)}`);
 });
