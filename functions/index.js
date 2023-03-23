78fd78908d376c5ff7f2762243d42c847d7b63e8
israelsaba <israelsaba@hotmail.com>
2023-03-23 15:57:36 -0300

    feat: Sends google ads data to MixPanel

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,13 @@ const functions = require("firebase-functions");
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+const url = require('url');
+const querystring = require('querystring');
+
+exports.sendMpDataAndForward = functions.https.onRequest((request, response) => {
+    const parsedURL = url.parse(request.url, true);
+    const queryData = querystring.parse(parsedURL.query);
+    response.status(200).send(`Data is: ${JSON.stringify(queryData, null, 2)}`);
+});
