dfeffb5356b00f34eb5f4a2e956cad554d66c248
israelsaba <israelsaba@hotmail.com>
2023-03-23 21:10:27 -0300

    feat: sets mixpanel production flag

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,9 @@ const functions = require("firebase-functions");
 const Mixpanel = require('mixpanel');
 const url = require('url');
 
+const workflow = { workflow: "production" }
+//const workflow = { workflow: "development" }
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
@@ -10,7 +13,7 @@ exports.sendMpAdsDataAndForward = functions.https.onRequest((request, response) 
     const parsedURL = url.parse(request.url, true);
     const queryParams = parsedURL.query
     const mixpanel = Mixpanel.init('73b777655a43f335b97bc9bb8cbf4700');
-    const combinedObj = Object.assign(parsedURL, queryParams);
+    const combinedObj = Object.assign(parsedURL, queryParams, workflow);
     mixpanel.track('Ad clicked', combinedObj);
     const forwardURL = `https://investorhub.app`
     response.redirect(forwardURL);
@@ -21,7 +24,7 @@ exports.sendMpASubscriberDataAndForward = functions.https.onRequest((request, re
     const parsedURL = url.parse(request.url, true);
     const queryParams = parsedURL.query
     const mixpanel = Mixpanel.init('73b777655a43f335b97bc9bb8cbf4700');
-    const combinedObj = Object.assign(parsedURL, queryParams);
+    const combinedObj = Object.assign(parsedURL, queryParams, workflow);
     if (combinedObj?.email) {
         //mixpanel.register({ Email: combinedObj.email, convertkitId: combinedObj.ck_subscriber_id });
         //mixpanel.identify(combinedObj.ck_subscriber_id);
@@ -40,7 +43,7 @@ exports.sendMpAEmailConfirmationDataAndForward = functions.https.onRequest((requ
     const parsedURL = url.parse(request.url, true);
     const queryParams = parsedURL.query
     const mixpanel = Mixpanel.init('73b777655a43f335b97bc9bb8cbf4700');
-    const combinedObj = Object.assign(parsedURL, queryParams);
+    const combinedObj = Object.assign(parsedURL, queryParams, workflow);
     mixpanel.track('Email confirmation clicked', combinedObj);
     const forwardURL = `https://calendly.com/israel-saba/investorhub-app`
     response.redirect(forwardURL);
